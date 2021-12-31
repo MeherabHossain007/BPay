b2450ac474117727834bcb487e80178a4168ddcd
MeherabHossain007 <meherab788@gmail.com>
2021-12-31 10:51:04 +0600

    footer logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ import {
     ChevronRightIcon,
   } from '@chakra-ui/icons';
   
-  export default function WithSubnavigation() {
+  export default function Navbar() {
     const { isOpen, onToggle } = useDisclosure();
   
     return (
@@ -252,41 +252,11 @@ import {
   
   const NAV_ITEMS: Array<NavItem> = [
     {
-      label: 'Inspiration',
-      children: [
-        {
-          label: 'Explore Design Work',
-          subLabel: 'Trending Design to inspire you',
-          href: '#',
-        },
-        {
-          label: 'New & Noteworthy',
-          subLabel: 'Up-and-coming Designers',
-          href: '#',
-        },
-      ],
-    },
-    {
-      label: 'Find Work',
-      children: [
-        {
-          label: 'Job Board',
-          subLabel: 'Find your dream design job',
-          href: '#',
-        },
-        {
-          label: 'Freelance Projects',
-          subLabel: 'An exclusive list for contract work',
-          href: '#',
-        },
-      ],
-    },
-    {
-      label: 'Learn Design',
+      label: 'Home',
       href: '#',
     },
     {
-      label: 'Hire Designers',
+      label: 'About',
       href: '#',
     },
   ];
